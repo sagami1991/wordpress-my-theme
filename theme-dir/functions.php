28d624335595b4377d813583225598a47c761927
socket1016 <socket1016@users.noreply.github.com>
2017-12-20 10:17:27 +0900

    no message

--- theme-dir/functions.php
+++ theme-dir/functions.php
@@ -24,8 +24,6 @@ if ( ! function_exists( 'pugiemonn_setup' ) ) :
 		 */
 		load_theme_textdomain( 'pugiemonn', get_template_directory() . '/languages' );
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
 
 		/*
 		 * Let WordPress manage the document title.
@@ -192,4 +190,23 @@ function get_post_source_badge($postId) {
 		</div>
 	<?php endif; 
 }
+
+// jquery無効
+function my_jquery_enqueue() {
+   wp_deregister_script('jquery');
+}
+if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
+
+// 絵文字無効
+function disable_emojis() {
+     remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+     remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+     remove_action( 'wp_print_styles', 'print_emoji_styles' );
+     remove_action( 'admin_print_styles', 'print_emoji_styles' );     
+     remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+     remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );  
+     remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+     add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+}
+add_action( 'init', 'disable_emojis' );
 ?>
--- theme-dir/header.php
+++ theme-dir/header.php
@@ -4,6 +4,8 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon.png">
 	<?php wp_head(); ?>
 </head>
 
